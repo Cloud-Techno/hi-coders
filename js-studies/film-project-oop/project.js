3650700a8eba0fd69ab8688e010ea994a6d5ad0d
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-08 10:09:10 +0300

    film project

---
+++ js-studies/film-project-oop/project.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+//Start UI Object
+
+const ui = new UI();
+
+// All Events 
+
+eventListeners();
+
+function eventListeners(){
+    form.addEventListener("submit",addFilm);
+
+}
+function addFilm(e){
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+        if (title === "" || director === "" || url === ""){
+           // Error
+        }
+        else{
+            //new Movie
+            const newFilm = new Film(title,director,url);
+
+            ui.addFilmToUI(newFilm); //add film for UI
+        }
+ui.clearInput(titleElement,urlElement,directorElement);
+
+    e.preventDefault();
+}
