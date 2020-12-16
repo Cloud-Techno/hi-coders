297ca52533f383d979527e75d85865686c403177
ferdiates <70488912+ferdiates@users.noreply.github.com>
2020-12-16 15:29:43 +0300

    calculate in array

--- home-works/calculate/app.js
+++ home-works/calculate/app.js
@@ -2,20 +2,20 @@ let numbers = [ 3,5,7,19,4,12,1];
 
 
 // find out square value in Array
-for (let index =0; index<numbers.length; index ++){
+for (let index =0; index<numbers.length; index++){
     let squareNumbers = Math.sqrt(numbers[index]);
     console.log("in Array" + (index+1)+'. number square is '+ squareNumbers);
 }
 
 // find out pow value in Array
-for (let index =0; index<numbers.length; index ++){
+for (let index =0; index<numbers.length; index++){
     let powNumbers = Math.pow(numbers[index], 2);
     console.log("in Array" + (index+1)+'. number pow= '+ powNumbers);
 }
 
 // find out sum in Array's values
 let sumNumbers = 0;
-for (let index =0; index<numbers.length; index ++){
+for (let index =0; index<numbers.length; index++){
     sumNumbers += numbers[index];
     console.log("Numbers of Array sum = "+ sumNumbers);
 }
@@ -29,7 +29,7 @@ for (let index =0; index<numbers.length; index ++){
     let min = numbers[0];
     let max = numbers[0];
 
-for (let index =0; index<numbers.length; index ++){
+for (let index =0; index<numbers.length; index++){
     if (numbers[index] < min){
             min = numbers[index];
     }
