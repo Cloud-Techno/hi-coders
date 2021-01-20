922681f7fc904d17c5c4a7ae5e8975c20eff7805
ferdiates <70488912+ferdiates@users.noreply.github.com>
2021-01-20 11:48:45 +0300

    battery studies

---
+++ battery/app.js
@@ -0,0 +1,35 @@
+/**
+ * bataryanin seviyesini gosteren bir islem yapilmasi
+ *  ilk once batarya seviyesinin hangi durumda oldugunu bilmem gerekiyor,
+ *  daha sonra batarya arttikca veya azaldikca, yani duruma gore sekil alan bir yapi olusturulmali,
+ *  duruma gore sekil degistiren yapiyi dinamik halde olmali,
+ */
+function updateUI(pEnerjiLevel,pBattaryContainer){
+    pBattaryContainer.className = `enerji-level-${pEnerjiLevel}`
+    }
+ //enerji seviyem 0 da basliyacak
+ let enerjiLevel = 0;
+
+// butonlari degisken icerisine atama
+const artanElement = document.getElementById('increaseButton');
+const azalanElement = document.getElementById('decreaseButton');
+
+// butonlara basildiginda uygulanacak olan function lari belirleme
+artanElement.addEventListener('click',doldur);
+azalanElement.addEventListener('click',azalt);
+
+//batarya elementini degiskene atama
+const batteryElement = document.getElementById('battery');
+function doldur(){
+    if(enerjiLevel<3){
+        enerjiLevel++;
+    }
+    updateUI(enerjiLevel,batteryElement)
+}
+
+function azalt(){
+    if(enerjiLevel > 0){
+        enerjiLevel--;
+    }
+    updateUI(enerjiLevel,batteryElement)
+}
